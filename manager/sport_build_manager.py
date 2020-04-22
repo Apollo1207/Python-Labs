94ced2d89caa4125c26d481090cb89f5e68c581f
Apollo1207 <fednyakillya12072001@gmail.com>
2020-04-22 15:02:49 +0300

    add sort|find methods and other fixes

--- manager/sport_build_manager.py
+++ manager/sport_build_manager.py
@@ -1,20 +1,18 @@
 class SportBuildManager:
-    def __init__(self, *args):
-        self.sport_builds = args
+    def __init__(self, sport_builds_list=None):
+        if sport_builds_list is None:
+            self.sport_builds_list = []
+        else:
+            self.sport_builds_list = sport_builds_list
 
-    def sort_by_name_of_sport(self):
-        pass
+    def sort_by_number_of_seats(self, reverse=True):
+        return sorted(self.sport_builds_list, key=lambda sport_build: sport_build.number_of_seats, reverse=reverse)
 
-    def sort_by_year_of_foundation(self):
-        pass
+    def sort_by_year_of_foundation(self, reverse=False):
+        return sorted(self.sport_builds_list, key=lambda sport_build: sport_build.year_of_foundation, reverse=reverse)
 
-    def sort_by_scale_of_field(self):
-        pass
+    def find_by_name_of_sport(self, name_of_sport):
+        return list(filter(lambda sport_build: sport_build.name_of_sport == name_of_sport, self.sport_builds_list))
 
-    def sort_by_number_of_seats(self):
-        pass
-
-    def find_by_number_of_seats(self):
-        pass
-
-
+    def find_by_scale_of_field(self, scale_of_field):
+        return list(filter(lambda sport_build: sport_build.scale_of_field == scale_of_field, self.sport_builds_list))
--- modules/main.py
+++ modules/main.py
@@ -1,19 +1,32 @@
-from classes.football_field import FootballField
-from classes.athletics_field import AthleticsField
-from classes.swimming_pool import SwimmingPool
-from classes.roof_type import RoofType
-from classes.purification_system_type import PurificationSystem
-from manager.sport_build_manager import SportBuildManager
+from classes import FootballField
+from classes import AthleticsField
+from classes import SwimmingPool
+from classes import RoofType
+from classes import PurificationSystem
+from manager import SportBuildManager
 
 
 def main():
-    sport_builds = [
-        FootballField(950, 2001, "Lviv", 35, "football", RoofType.OPEN, "green", 52),
-        AthleticsField(950, 2001, "Dnipro", 35, "athletics", 18, 9, "rubber"),
-        SwimmingPool(950, 2001, "Kyiv", 35, "swimming", 2, 9, PurificationSystem.ULTRAVIOLET),
+    sport_builds_list = [
+        FootballField(1, 1, "Lviv", 1, "football", RoofType.OPEN, "green", 52),
+        AthleticsField(2, 2, "Dnipro", 2, "athletics", 18, 9, "rubber"),
+        SwimmingPool(3, 3, "Kyiv", 3, "swimming", 2, 9, PurificationSystem.ULTRAVIOLET)
     ]
 
-    manager = SportBuildManager(*sport_builds)
+    sport_build_manager = SportBuildManager(sport_builds_list)
+
+    method_calls_list = [sport_build_manager.sort_by_number_of_seats(),
+                         sport_build_manager.sort_by_year_of_foundation(),
+                         sport_build_manager.find_by_name_of_sport(name_of_sport="swimming"),
+                         sport_build_manager.find_by_scale_of_field(scale_of_field=2)
+                         ]
+
+    output_processed_lists(method_calls_list)
+
+
+def output_processed_lists(method_calls_list):
+    for obj in method_calls_list:
+        print("".join(str(x) for x in obj))
 
 
 if __name__ == "__main__":
